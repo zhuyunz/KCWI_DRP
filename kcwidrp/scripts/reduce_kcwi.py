e25e299762377b6e0318d1a3618c90632184ae31
Yuguang Chen <yuguang.chen.1@gmail.com>
2023-01-23 15:43:41 -0500

    Add testing_icube stage

--- kcwidrp/scripts/reduce_kcwi.py
+++ kcwidrp/scripts/reduce_kcwi.py
@@ -166,6 +166,9 @@ def main():
     elif args.stage=='sine':
         from kcwidrp.pipelines.kcwi_pipeline_testing_sine_pattern import Kcwi_pipeline
         print('KCWI Pipeline Testing Sine Pattern')
+    elif args.stage=='icube':
+        from kcwidrp.pipelines.kcwi_pipeline_testing_icube import Kcwi_pipeline
+        print('KCWI Pipeline IDL Stage 1-6')
     else:
         from kcwidrp.pipelines.kcwi_pipeline import Kcwi_pipeline
         print("Invalid stage choice [0..3]: going with default (full) reduction")
